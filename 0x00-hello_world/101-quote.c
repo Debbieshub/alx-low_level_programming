af820723b81eb0c84e4bbf84dc8910f3f0e2895b
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-02 02:16:57 -0700

    making corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	putchar("and thats piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("and thats piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
