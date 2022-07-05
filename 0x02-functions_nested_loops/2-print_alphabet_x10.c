81c9d8a2723bfcae07c206fda62b04bece378b42
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-05 14:14:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print x10 of the alphabet in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
