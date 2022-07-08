196ccf88e2cc3fa8d59742a4f608103cdc472fc2
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-08 11:59:06 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,16 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9 in a new line
+ *
  */
 
 void print_numbers(void)
 {
 	int x;
 
-	for (x <= '0'; x >= '9'; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		_putchar(c);
+		_putchar(x);
 	}
 	_putchar('\n');
 }
