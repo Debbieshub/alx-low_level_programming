f264cb16410b11b135a0444c0ef8e86f5dcb4d71
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-08 12:25:51 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,11 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9 followed by a new line
  * Except 2 and 4
- *
- * Return: 0 if successful
  */
 
 void print_most_numbers(void)
 {
-	int i = '0';
+	int i = 0;
 
 	while (i <= '9')
 	{
@@ -20,5 +18,4 @@ void print_most_numbers(void)
 		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
