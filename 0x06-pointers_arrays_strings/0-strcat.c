7c5ecbd68df98b714775274ff2d56128d54e4c95
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-14 10:09:27 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - contactenating 2 strings
+ * @dest: first declared string
+ * @src: second declared string
+ *
+ * Return: 0(if successful)
+ */
+char *_strcat(char *dest, char *src);
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
