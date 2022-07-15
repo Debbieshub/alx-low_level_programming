9ce2e7bb78b3573819444d0efc0f1762a99e5696
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-15 15:33:11 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
-* rot13 - encodes a string using rot13.
-* @s: The string
-*
-* Return: The string
-*/
-char *rot13(char *s)
+int main(void)
 {
-	int i, j;
-	char r[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char boolean;
+  int n;
+  int a[5];
+  int *p;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		boolean = 0;
-		for (j = 0; alpha[j] != '\0' && boolean == 0; j++)
-		{
-			if (s[i] == alpha[j])
-			{
-				s[i] = r[j];
-				boolean = 1;
-			}
-		}
-	}
-
-	return (s);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
