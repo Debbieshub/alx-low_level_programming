fecc868dd63701ae813ac653c40162e4cace6fa5
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-18 15:08:04 -0700

    2-strchr.c2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,8 +4,9 @@
  * _strchr - locating a character in a string
  * @s: string of characters
  * @c: character variable to search
+ *
  * Return: pointer to the first occurence of the character c in the string s
- */
+ **/
 char *_strchr(char *s, char c)
 {
 	int i;
