172a02bc339e633f8ed187ca99cc39773937d097
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-07-20 13:19:42 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - printing a string followed by a new line
+ * @s: The string to print
+ *
+ * Return: s
+ */
+void _puts_recursion(char *s);
+{
+	int i;
+	if (i == 0)
+	{
+		return 0;
+	}
+
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
