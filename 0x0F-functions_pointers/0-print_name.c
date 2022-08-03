f51eae45bf9372001855273a7e296150d3d09e65
Debbieshub <aruwajoyedeborah005@gmail.com>
2022-08-03 08:16:40 -0700

    0-print_name.c

---
+++ 0x0F-functions_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_name - writing a function that prints a name
+ * @name: string of characters or name to be printed
+ * @f: function pointer that takes a string arguement
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
